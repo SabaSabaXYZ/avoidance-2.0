a69cde3292fc4a18b2cf48b88c70cc0995fca6d9
Saba Saba <saba@sabadev.xyz>
2026-02-06 07:57:47 -0500

    Factored out rendering and key-handling logic.

--- src/main.c
+++ src/main.c
@@ -10,6 +10,10 @@ typedef enum GameScreen {
   HELP
 } GameScreen;
 
+typedef struct GameState {
+  GameScreen screen;
+} GameState;
+
 void DrawTextCentre(const char *text, int posY, Color color) {
   int textWidth = MeasureText(text, FONT_SIZE);
   DrawText(text, (SCREEN_WIDTH - textWidth) / 2, posY, FONT_SIZE, color);
@@ -21,25 +25,49 @@ void DrawTitle() {
   DrawTextCentre("Move: W/A/S/D", startHeight + FONT_SIZE * 2, WHITE);
   DrawTextCentre("Help: H", startHeight + FONT_SIZE * 4, WHITE);
   DrawTextCentre("Play: P", startHeight + FONT_SIZE * 6, WHITE);
-  DrawTextCentre("Quit: Q", startHeight + FONT_SIZE * 8, WHITE);
+  DrawTextCentre("Quit: Q/Esc", startHeight + FONT_SIZE * 8, WHITE);
+}
+
+void HandleDraw(GameState state) {
+  BeginDrawing();
+  ClearBackground(BLACK);
+  switch (state.screen) {
+    case TITLE:
+      DrawTitle();
+      break;
+    default:
+      break;
+  }
+  EndDrawing();
+}
+
+bool HandleKeyPress(GameState state) {
+  switch (state.screen) {
+    case TITLE:
+      if (IsKeyPressed(KEY_Q)) {
+        return true;
+      }
+      if (IsKeyPressed(KEY_F)) {
+        ToggleFullscreen();
+      }
+      break;
+    default:
+      break;
+  }
+  return false;
 }
 
 int main(void) {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Avoidance");
   SetTargetFPS(60);
   ToggleFullscreen();
-  GameScreen gameScreen = TITLE;
+  GameState state;
+  state.screen = TITLE;
   while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(BLACK);
-    switch (gameScreen) {
-      case TITLE:
-        DrawTitle();
-        break;
-      default:
-        break;
+    HandleDraw(state);
+    if (HandleKeyPress(state)) {
+      break;
     }
-    EndDrawing();
   }
   CloseWindow();
   return 0;
