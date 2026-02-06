15a1ac6f201740e75710487c20c6b1e3e6f3617d
Saba Saba <saba@sabadev.xyz>
2026-02-05 20:19:28 -0500

    Working on title screen

--- src/main.c
+++ src/main.c
@@ -1,8 +1,45 @@
 #include <raylib.h>
 
-int main(int argc, char **argv) {
-  InitWindow(500, 500, "Avoidance");
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 800
+#define FONT_SIZE 20
+
+typedef enum GameScreen {
+  TITLE = 0,
+  GAMEPLAY,
+  HELP
+} GameScreen;
+
+void DrawTextCentre(const char *text, int posY, Color color) {
+  int textWidth = MeasureText(text, FONT_SIZE);
+  DrawText(text, (SCREEN_WIDTH - textWidth) / 2, posY, FONT_SIZE, color);
+}
+
+void DrawTitle() {
+  int startHeight = SCREEN_HEIGHT / 4;
+  DrawTextCentre("==Avoidance==", startHeight, WHITE);
+  DrawTextCentre("Move: W/A/S/D", startHeight + FONT_SIZE * 2, WHITE);
+  DrawTextCentre("Help: H", startHeight + FONT_SIZE * 4, WHITE);
+  DrawTextCentre("Play: P", startHeight + FONT_SIZE * 6, WHITE);
+  DrawTextCentre("Quit: Q", startHeight + FONT_SIZE * 8, WHITE);
+}
+
+int main(void) {
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Avoidance");
+  SetTargetFPS(60);
+  ToggleFullscreen();
+  GameScreen gameScreen = TITLE;
   while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    switch (gameScreen) {
+      case TITLE:
+        DrawTitle();
+        break;
+      default:
+        break;
+    }
+    EndDrawing();
   }
   CloseWindow();
   return 0;
