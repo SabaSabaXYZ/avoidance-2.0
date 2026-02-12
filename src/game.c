5a0415e70808bb73420fd8564eaf360d376360a2
Saba Saba <saba@sabadev.xyz>
2026-02-12 18:42:51 -0500

    Got random enemy directions implemented

--- src/game.c
+++ src/game.c
@@ -1,5 +1,7 @@
 #include "game.h"
 #include <raymath.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 static Rectangle VectorToRectangle(GameState *state, Vector2 vector) {
   return (Rectangle) {
@@ -54,6 +56,35 @@ static void CheckGameEnd(GameState *state) {
   }
 }
 
+static void InitializeEnemy(GameState *state, Vector2 *position, MoveDirection *direction) {
+  if (position->x >= state->gameBoundary.x && position->x <= state->gameBoundary.width && position->y >= state->gameBoundary.y && position->y <= state->gameBoundary.height) {
+    return;
+  }
+  *direction = (MoveDirection) GetRandomValue(UP, RIGHT);
+  switch (*direction) {
+    case UP:
+      position->x = GetRandomValue(state->gameBoundary.x, state->gameBoundary.width);
+      position->y = state->gameBoundary.height;
+      break;
+    case DOWN:
+      position->x = GetRandomValue(state->gameBoundary.x, state->gameBoundary.width);
+      position->y = state->gameBoundary.y;
+      break;
+    case LEFT:
+      position->x = state->gameBoundary.width;
+      position->y = GetRandomValue(state->gameBoundary.y, state->gameBoundary.height);
+      break;
+    case RIGHT:
+      position->x = state->gameBoundary.x;
+      position->y = GetRandomValue(state->gameBoundary.y, state->gameBoundary.height);
+      break;
+    default:
+      perror("Invalid MoveDirection defined for enemy");
+      exit(EXIT_FAILURE);
+      break;
+  }
+}
+
 void StartGame(GameState *state) {
   state->characterCount = 3;
   state->score = 0;
@@ -72,12 +103,15 @@ void UpdatePositions(GameState *state) {
     return;
   }
   state->score++;
+  for (unsigned int i = ENEMY_START_ID; i < state->characterCount; ++i) {
+    InitializeEnemy(state, &state->positions[i], &state->directions[i]);
+  }
   for (unsigned int i = 0; i < state->characterCount; ++i) {
     UpdatePositionByDirection(state, &state->positions[i], state->directions[i]);
   }
   UpdateBoxDirection(state);
   CheckGameEnd(state);
-  for (unsigned int i = PLAYER_ID; i < state->characterCount; ++i) {
+  for (unsigned int i = PLAYER_ID; i < ENEMY_START_ID; ++i) {
     RestrictToGameArea(state, &state->positions[i]);
   }
 }
