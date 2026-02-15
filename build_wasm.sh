6635b97c94ce579dc0d662491d66ce16606263d1
Saba Saba <saba@sabadev.xyz>
2026-02-14 20:11:49 -0500

    Renamed html output to index.html

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-# emcmake cmake -DCMAKE_TOOLCHAIN_FILE=../../emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DPLATFORM=Web -B .
-emcc src/*.c -o Avoidance2.html -L../raylib/src -I../raylib/src -l:libraylib.web.a -s USE_GLFW=3 -s ASYNCIFY -s STACK_SIZE=67108864
+emcc src/*.c -o index.html -L../raylib/src -I../raylib/src -l:libraylib.web.a -s USE_GLFW=3 -s ASYNCIFY -s STACK_SIZE=67108864
